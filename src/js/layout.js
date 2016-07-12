2fbac1b3dd72e99b365d929b1f4dc8e084de9c8a
Daniel Price <dprice@rpco-mwhite1508.rpcorp.local>
2016-07-12 16:58:47 -0600

    styles

--- src/js/layout.js
+++ src/js/layout.js
@@ -10,14 +10,9 @@ try {
   /* load pattern nav */
   var template         = document.getElementById("pl-pattern-nav-template");
   var templateCompiled = Hogan.compile(template.innerHTML);
+  console.log(navItems);
   var templateRendered = templateCompiled.render(navItems);
   document.getElementById("pl-pattern-nav-target").innerHTML = templateRendered;
-  
-  /* load ish controls */
-  var template         = document.getElementById("pl-ish-controls-template");
-  var templateCompiled = Hogan.compile(template.innerHTML);
-  var templateRendered = templateCompiled.render(ishControls);
-  document.getElementById("sg-controls").innerHTML = templateRendered;
   
 } catch(e) {
   
